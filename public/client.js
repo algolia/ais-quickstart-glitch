b70f552350c7d4ec7b172cac90bff06d31ca379a
Jessica West <jgoulding2@gmail.com>
2017-12-12 12:38:20 -0800

    Instructions feedback (#4)
    * save wip
    
    * variables partial to hold data from server side variables set, more helper text in algolia.js file

--- public/client.js
+++ public/client.js
@@ -1,1 +1,23 @@
+// the object set by the server containing valuable configuration info
+var glitchApp = window.glitchApp;
 
+// check if Algolia API keys are in the .env file
+if (glitchApp.data.algolia_env == true) {
+	document.getElementById("step-1").classList.add('completed-steps');
+	document.getElementById("step-1").classList.remove('current-step');
+	document.getElementById("step-2").classList.add('current-step-steps');
+}
+
+// check if the domain has changed from instantsearch-quickstart
+if (glitchApp.data.new_domain == true) {
+	document.getElementById("step-2").classList.add('completed-steps');
+	document.getElementById("step-2").classList.remove('current-step');
+	document.getElementById("step-3").classList.add('current-step-steps');	
+}
+
+// check if .setSettings has been enabled
+if (glitchApp.data.query_data == true) {
+  document.getElementById("step-3").classList.add('completed-steps');
+  document.getElementById("step-3").classList.remove('current-step');
+  document.getElementById("step-4").classList.add('current-step-steps');  
+}
--- server.js
+++ server.js
@@ -9,16 +9,18 @@ const dataUrl = 'http://api.giphy.com/v1/gifs/search?q=dogs&api_key=dc6zaTOxFJmz
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
 
+// we use nunjucks to send data that we can later use in our views
 nunjucks.configure('views', {
   express: app,
   noCache: true
 });
 
-// // index route
+// create index route
 app.get('/', (request, response) => {
   response.send(nunjucks.render('index.html', getTemplateContext(request)));
 });
 
+// create search route
 app.get('/search', (request, response) => {
   response.send(nunjucks.render('search.html', getTemplateContext(request)));
 })
@@ -32,9 +34,51 @@ function getTemplateContext(request) {
       index_name: process.env.PROJECT_DOMAIN,
       app_id: process.env.ALGOLIA_APP_ID,
       search_api_key: process.env.ALGOLIA_SEARCH_API_KEY
+    },
+    data: {
+      algolia_env: algoliaClient(),
+      new_domain: newDomainCheck(),
+      query_data: queryDataCheck()
     }
   };
 }
+
+// helper methods to check server side data for users set up checklist
+
+// check to see if user has remixed the application
+function newDomainCheck() {
+  if (process.env.PROJECT_DOMAIN != 'instantsearch-quickstart') {
+    console.log(process.env.PROJECT_DOMAIN)
+    return true;
+  } else {
+    console.warn('Project has not been remixed yet.');
+    return null;
+  }
+}
+
+// check to see if user has added variables to their .env file
+function algoliaClient() {
+  if (process.env.ALGOLIA_APP_ID &&
+      process.env.ALGOLIA_ADMIN_API_KEY &&
+      process.env.ALGOLIA_SEARCH_API_KEY) {
+    return true;
+  } else {
+    console.warn('One or more Algolia environment variables missing.');
+    return null;
+  }
+}
+
+// check to see if user has added a data source and sent data to Aloglia for indexing
+function queryDataCheck() {
+  if (algoliaHelper.indexTweets(dataUrl).length != 0){
+    console.log(algoliaHelper.indexTweets(dataUrl).length)
+    return true;
+  } else {
+    console.warn('Project has not set up indexed data yet.');
+    return null;
+  }
+}
+
 // listen for requests :)
 const listener = app.listen(process.env.PORT, function () {
   console.log('Your app is listening on port ' + listener.address().port);
