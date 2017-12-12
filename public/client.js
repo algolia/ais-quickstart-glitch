ef5f50c496c6bd115d2f1143761da727722f30e2
Jessica West <jgoulding2@gmail.com>
2017-12-11 18:21:44 -0800

    save wip

--- public/client.js
+++ public/client.js
@@ -1,1 +1,20 @@
+// check if Algolia API keys are in the .env file
+if (instantSearch.algoliaEnvDetected) {
+	document.getElementById("step-1").classList.add('completed-steps');
+	document.getElementById("step-1").classList.remove('current-step');
+	document.getElementById("step-2").classList.add('current-step-steps');
+}
 
+// check if the domain has changed from instantsearch-quickstart
+if (instantSearch.newDomainDetected) {
+	document.getElementById("step-2").classList.add('completed-steps');
+	document.getElementById("step-2").classList.remove('current-step');
+	document.getElementById("step-3").classList.add('current-step-steps');	
+}
+
+// check if .setSettings has been enabled
+if (instantSearch.newDomainDetected) {
+  document.getElementById("step-3").classList.add('completed-steps');
+  document.getElementById("step-3").classList.remove('current-step');
+  document.getElementById("step-4").classList.add('current-step-steps');  
+}
