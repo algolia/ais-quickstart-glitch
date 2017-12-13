7b36973ea78fcb3ad02bc617f1793bb2e22a8ea6
Jessica West <jgoulding2@gmail.com>
2017-12-12 17:03:50 -0800

    more details in code, fixing logic for display

--- public/client.js
+++ public/client.js
@@ -1,23 +1,26 @@
-// the object set by the server containing valuable configuration info
-var glitchApp = window.glitchApp;
+$(document).ready(function() {
+  // the object set by the server containing valuable configuration info
+  var glitchApp = window.glitchApp;
+  
+  // check if the domain has changed from instantsearch-quickstart
+  if (glitchApp.data.new_domain) {
+    document.getElementById("step-1").classList.add('completed-steps');
+    document.getElementById("step-1").classList.remove('current-step');
+    document.getElementById("step-2").classList.add('current-step');  
+  }
+  
+  // check if Algolia API keys are in the .env file
+  if (glitchApp.data.algolia_env) {
+    document.getElementById("step-2").classList.add('completed-steps');
+    document.getElementById("step-2").classList.remove('current-step');
+    document.getElementById("step-3").classList.add('current-step');
+  }
 
-// check if Algolia API keys are in the .env file
-if (glitchApp.data.algolia_env == true) {
-	document.getElementById("step-1").classList.add('completed-steps');
-	document.getElementById("step-1").classList.remove('current-step');
-	document.getElementById("step-2").classList.add('current-step-steps');
-}
-
-// check if the domain has changed from instantsearch-quickstart
-if (glitchApp.data.new_domain == true) {
-	document.getElementById("step-2").classList.add('completed-steps');
-	document.getElementById("step-2").classList.remove('current-step');
-	document.getElementById("step-3").classList.add('current-step-steps');	
-}
-
-// check if .setSettings has been enabled
-if (glitchApp.data.query_data == true) {
-  document.getElementById("step-3").classList.add('completed-steps');
-  document.getElementById("step-3").classList.remove('current-step');
-  document.getElementById("step-4").classList.add('current-step-steps');  
-}
+  // check if data url is not blank
+  if (glitchApp.data.query_data) {    
+    document.getElementById("step-3").classList.add('completed-steps');
+    document.getElementById("step-3").classList.remove('current-step');
+    document.getElementById("step-4").classList.add('current-step');  
+  }
+  
+});
--- server/helpers/algolia.js
+++ server/helpers/algolia.js
@@ -14,13 +14,15 @@ const algoliasearch = require('algoliasearch');
 // instantiate constant for algoliasearch passing in variables needed at setup
 const algoliaClient = algoliasearch(process.env.ALGOLIA_APP_ID, process.env.ALGOLIA_ADMIN_API_KEY);
 
-// currently using project domain from glitch ie dancing-squirrel.
+// (~˘▽˘)~ Useful tips
+// index name is currently using project domain from glitch ie dancing-squirrel
 // change within parens of initIndex for something relevant to the data you want to index
 // good examples are 'movie-actors' or 'dog-pictures' or 'my-contacts'
-// you can find this index created under your Algolia dashboard => Indices
+// you can find this index created under your Algolia dashboard => indices
 const algoliaIndex = algoliaClient.initIndex(process.env.PROJECT_DOMAIN);
 
-// call this function from the server to start indexing data
+// (~˘▽˘)~ Useful tips
+// call this function from the server.js to start indexing data
 // the data url received is in JSON format, so we call .data on it to start working with it
 // axios by default uses promises, so we can chain .then on our functions
 // we use dataToAlgoliaObject to create usable algoliaObjects in our index
@@ -28,9 +30,16 @@ const algoliaIndex = algoliaClient.initIndex(process.env.PROJECT_DOMAIN);
 function indexTweets(data_url){
   return axios.get(data_url,{})
   .then(function(response){
-      //return dataToAlgoliaObject(response.data.data)
+      // *＼(＾▽＾)／ 🔎 Step 3a: Comment in this line below
+      // console.log(response.data)
+      // look at your data, are you ready to loop over as is and assign?
+      // do you need to go one step further? call that here before sending to the dataToAlgoliaObject
+    
+      // *＼(＾▽＾)／ 🔎 Step 3c: comment in this line when ready :)
+      // return dataToAlgoliaObject(response.data.data)
     })
   .then(function(response){
+     // *＼(＾▽＾)／ 🔎 Step 3d: Comment in this line whe
      //sendDataToAlgolia(response)
   })
   .catch(function(error) {
@@ -38,6 +47,7 @@ function indexTweets(data_url){
     })
 }
 
+// (~˘▽˘)~ Useful tips
 // the data_points we have is still raw with many objects underneath data
 // we start with an empty array "algoliaObjects"
 // we loop over each data point until we reach the end
@@ -45,25 +55,24 @@ function indexTweets(data_url){
 // you can see the title for example by looping through the objects by calling algoliaObject.title indvididually
 // change the variable names in here according to what makes sense with the data you are using
 // required and best practice for Algolia is to have objectID be the same as the datapoint id 
+
 function dataToAlgoliaObject(data_points){
-	var algoliaObjects = [];
+  var algoliaObjects = [];
   console.log("looping over data points: " + data_points.length)
   // iterate over data and build the algolia record
   for (var i = 0; i < data_points.length; i++) {
     var data_point = data_points[i];
     var algoliaObject = {
+        // (~˘▽˘)~ Useful tips
         // the objectID is the key for the algolia record, and mapping
         // data id to object ID guarantees only one copy of the data in algolia
         // change these variables pending on your data source and how you want to display data
         objectID: data_point.id,
         id: data_point.id,
-        title: data_point.title.replace(/GIF/g, ''),
         // we use the Date.parse and divide by 1000 to get a human readable date to use
         created_at: Date.parse(data_point.import_datetime) / 1000,
-        trending_date: Date.parse(data_point.trending_datetime) / 100,
-        embed_url: data_point.embed_url,
-        rating: data_point.rating 
       };
+      // (~˘▽˘)~ Useful tips
       // we can see our objects being created in the console!
       // use console.log for debugging while you are learning and creating
       // feel free to remove/comment out when you are done.
@@ -80,31 +89,33 @@ function dataToAlgoliaObject(data_points){
   return algoliaObjects;
 }
 
-// 
-function sendDataToAlgolia(algoliaObjects){
-  console.log("Sending data to Algolia" + algoliaObjects)
-  algoliaIndex.addObjects(algoliaObjects, function(err, content) {
-    console.log(content);
-  });
-}
-
+// (~˘▽˘)~ Useful tips
 // there are a lot of options for your setSettings on how you want to see your data
 // using the constant we defined earlier for initiated the index, we call setSettings
 // there are a lot of options all listed here: https://www.algolia.com/doc/api-reference/api-parameters/
 
-///// Basic Setup:
+// (~˘▽˘)~ Basic Setup:
 // searchableAttributes: list here what you would like searchable from the algoliaObject you created
 // customRanking: choose how your data will be displayed, desc() or asc()
 // attributesToHighlight: highlights the text you are searching for
 // attributesToRetrieve: return these attributes for dislaying in search results
 // ignorePlurals: make plural and singular matches count the same for languages you specify
+
 function configureAlgoliaIndex(){
   console.log("Configuring index")
+  // *＼(＾▽＾)／ 🔎 Step 3b: Comment in the lines for which settings you want to use
   algoliaIndex.setSettings({
-  searchableAttributes: [
-    'title'
-  ],
-  customRanking: ['desc(rating)'],
+  // searchableAttributes: [
+  //   'title'
+  // ],
+  // customRanking: ['desc(rating)'],
+  });
+}
+
+function sendDataToAlgolia(algoliaObjects){
+  console.log("Sending data to Algolia" + algoliaObjects)
+  algoliaIndex.addObjects(algoliaObjects, function(err, content) {
+    console.log(content);
   });
 }
 
